6c6cdb0b45840cb15ef7191c312593252cbac575
Walid Arnous <walidarnous.1@gmail.com>
2023-06-01 09:46:12 -0300

    Usando o Select

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -93,6 +93,26 @@ describe('Central de Atendimento ao Cliente TAT', function() {
 
         cy.get('.success').should('be.visible')
     })
-
+    
+    //#Pelo texto
+    it('seleciona um produto (YouTube) por seu texto', function() {
+        cy.get('#product')
+        .select('YouTube')
+        .should('have.value', 'youtube')
+    })
+    
+    //#Pelo value
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+        cy.get('#product')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
+    })
+    
+    //#Pelo indice
+    it.only('seleciona um produto (Blog) por seu índice', function(){
+        cy.get('#product')
+        .select(1)
+        .should('have.value', 'blog')
+    })
 })
   
